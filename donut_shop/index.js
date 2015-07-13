a579e50a359c3dd6cbe37dfeb8f24024b98547d8
Emre Surmeli <s.emre.s.8@gmail.com>
2015-07-13 12:48:24 -0700

    we be making sum donuts

---
+++ donut_shop/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var DonutShop = function (minCustHr. maxCustHr, avgDonutPC) {
+  this.minCustHr = minCustHr;
+  this.maxCustHr = maxCustHr;
+  this.avgDonutPC = avgDonutPC;
+  this.render = function () {
+    // render element for DOM
+  }
+ }
+
+var DonutShop.prototype.donutPerHr = function () {
+  // create donut per hour
+}
+
+var DonutShop.prototype.donutPerDay = function () {
+  // create donut per day
+}
+
+var downTown = new DonutShop (10, 40, 1.5);
+var eastSide = new DonutShop (5, 30, 1);
+var southEnd = new DonutShop (10, 60, 2);
+var westSide = new DonutShop (5, 45, 1);
+var airPort = new DonutShop (15, 100, 2);
