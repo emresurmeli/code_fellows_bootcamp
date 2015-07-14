5960f2a8410e6f0509400d077ef0bc011547469b
Emre Surmeli <s.emre.s.8@gmail.com>
2015-07-13 19:01:48 -0700

    render function left

--- donut_shop/index.js
+++ donut_shop/index.js
@@ -1,38 +1,37 @@
 'use strict'
 
 // Create a donut shop constructor
-var DonutShop = function (minCustHr, maxCustHr, avgDonutPC) {
-  this.minCustHr = minCustHr;
-  this.maxCustHr = maxCustHr;
+var DonutShop = function (name, minCustHr, maxCustHr, avgDonutPC) {
+  this.name       = name;
+  this.minCustHr  = minCustHr;
+  this.maxCustHr  = maxCustHr;
   this.avgDonutPC = avgDonutPC;
-  this.render = function () {
-    return this.donutPerDay();
+  this.rander = function() {
+    // make render function
   }
 }
 
-// return total donuts sold per hour based on 11 hours of operation
-DonutShop.prototype.donutPerHr = function () {
-  var hrsArr = [];
+// Return total donuts sold per hour based on 11 hours of operation
+DonutShop.prototype.donutPerHr = function() {
+  var avgDonutHr = [];
   for(var i = 0; i < 11; i++) {
-   hrsArr.push(Math.floor(Math.random() * this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgDonutPC;
+   avgDonutHr.push(Math.floor(Math.random() * this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgDonutPC;
   }
-  return hrsArr;
+  return avgDonutHr;
 }
 
-// return total donuts sold per day based on 11 hours of operation
-DonutShop.prototype.donutPerDay = function () {
-  var hrsArr = this.donutPerHr();
+// Return total donuts sold per day based on 11 hours of operation
+DonutShop.prototype.donutPerDay = function() {
+  var avgDonutHr = this.donutPerHr();
   var total = 0;
-  for(var i = 0; i < hrsArr.length; i++) {
-    total += hrsArr[i];
+  for(var i = 0; i < avgDonutHr.length; i++) {
+    total += avgDonutHr[i];
   }
   return total;
 }
 
-var downTown = new DonutShop (10, 40, 1.5);
-var eastSide = new DonutShop (5, 30, 1);
-var southEnd = new DonutShop (10, 60, 2);
-var westSide = new DonutShop (5, 45, 1);
-var airPort = new DonutShop (15, 100, 2);
-
-console.log(downTown.render());
+var dT = new DonutShop ('Downtown', 10, 40, 1.5);
+var eS = new DonutShop ('East Side', 5, 30, 1);
+var sE = new DonutShop ('South End', 10, 60, 2);
+var wS = new DonutShop ('West Side', 5, 45, 1);
+var aP = new DonutShop ('Air Port', 15, 100, 2);
